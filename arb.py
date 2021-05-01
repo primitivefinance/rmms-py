950078a91c497bdc586a2ee12f84f20aacf018f6
experiencedft <14246657+experiencedft@users.noreply.github.com>
2021-05-01 19:19:33 +0200

    Prevent arbitrage attempts near boundaries

--- arb.py
+++ arb.py
@@ -22,22 +22,22 @@ class Arbitrager():
         '''
         #Check which asset we'll have to swap in to arbitrage
         amm_spot_price = Pool.getSpotPrice()
-        if amm_spot_price > reference_price:
+        if amm_spot_price > reference_price and Pool.reserves_risky < 0.999:
             #Swap the risky asset for the riskless asset to move the price down
             #Find the smallest swap that would satisfy amm_spot < reference_price using virtual swaps
             spot_price_after_trade = amm_spot_price
-            amount_in = 0.001
-            while spot_price_after_trade > reference_price:
+            amount_in = 0.00001
+            while spot_price_after_trade > reference_price and amount_in + Pool.reserves_risky < 0.999:
                 spot_price_after_trade = Pool.getSpotPriceAfterVirtualSwapAmountInRisky(amount_in)
-                amount_in += 0.001
+                amount_in += 0.00001
             #Actually perform the swap
             _ = Pool.swapAmountInRisky(amount_in)
-        else:
+        elif amm_spot_price < reference_price and Pool.reserves_riskless < 999.9:
             #Swap the riskless asset for the risky asset to move the price up
             spot_price_after_trade = amm_spot_price
-            amount_in = 1
-            while spot_price_after_trade < reference_price:
+            amount_in = 0.1
+            while spot_price_after_trade < reference_price and amount_in + Pool.reserves_riskless < 999:
                 spot_price_after_trade = Pool.getSpotPriceAfterVirtualSwapAmountInRiskless(amount_in)
-                amount_in += 1
+                amount_in += 0.1
             #Actually perform the swap
             _ = Pool.swapAmountInRiskless(amount_in)
