1b9f74e29c212fdb6dfb9a3592a026f82c9dd68f
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-04-27 10:38:04 -0600

    clean

--- modules/arb.py
+++ modules/arb.py
@@ -1,6 +1,6 @@
-'''
+"""
 Arbitrage function
-'''
+"""
 
 import scipy
 import numpy as np
@@ -8,9 +8,11 @@ from scipy import optimize
 
 EPSILON = 1e-8
 
-def arbitrageExactly(market_price, Pool):
-    '''
-    Arbitrage the difference *exactly* at the time of the call to the function. Uses results from the following paper: https://arxiv.org/abs/2012.08040
+
+def arbitrageExactly(market_price, pool):
+    """
+    Arbitrage the difference *exactly* at the time of the call to the function.
+    Uses results from the following paper: https://arxiv.org/abs/2012.08040
 
     Params:
 
@@ -18,30 +20,29 @@ def arbitrageExactly(market_price, Pool):
         the reference price of the risky asset, denominated in the riskless asset
     Pool (AMM object):
         an AMM object, for example a CoveredCallAMM class, with some current state and reserves
-    '''
-    gamma = 1 - Pool.fee
-    R1 = Pool.reserves_risky
-    R2 = Pool.reserves_riskless
-    K = Pool.K
-    k = Pool.invariant
-    sigma = Pool.sigma
-    tau = Pool.tau
+    """
+    gamma = 1 - pool.fee
+    R1 = pool.reserves_risky
+    R2 = pool.reserves_riskless
+    K = pool.K
+    k = pool.invariant
+    sigma = pool.sigma
+    tau = pool.tau
 
-
-    #Marginal price of selling epsilon risky
+    # Marginal price of selling epsilon risky
     # price_sell_risky = gamma*K*norm.pdf(norm.ppf(1 - R1) - sigma*np.sqrt(tau))*quantilePrime(1 - R1)
-    price_sell_risky = Pool.getMarginalPriceSwapRiskyIn(0)
-    #Marginal price of buying epsilon risky
-    price_buy_risky = Pool.getMarginalPriceSwapRisklessIn(0)
+    price_sell_risky = pool.getMarginalPriceSwapRiskyIn(0)
+    # Marginal price of buying epsilon risky
+    price_buy_risky = pool.getMarginalPriceSwapRisklessIn(0)
 
-    #Market price
+    # Market price
     m = market_price
 
     # If the risky reserves are almost empty
-    if R1 < EPSILON: 
+    if R1 < EPSILON:
         return
     # or if the riskless reserves are almost empty
-    elif R2 < EPSILON or (K+k-R2)/gamma < EPSILON: 
+    elif R2 < EPSILON or (K + k - R2) / gamma < EPSILON:
         return
 
     # or if the risky reserves are almost full
@@ -52,37 +53,39 @@ def arbitrageExactly(market_price, Pool):
     elif K - R2 < EPSILON:
         return
 
-    #In any of the above cases, we do nothing, this ensures that the bracketing for the root finding will always test a positive amount in.
+    # In any of the above cases, we do nothing, this ensures that the bracketing for the root finding will always test a positive amount in.
 
-    #If the price of selling epsilon of the risky asset is above the market price, we buy the optimal amount of the risky asset on the market and immediately sell it on the CFMM = **swap amount in risky**.
+    # If the price of selling epsilon of the risky asset is above the market price, we buy the optimal amount of the risky asset on the market and immediately sell it on the CFMM = **swap amount in risky**.
     elif price_sell_risky > m + 1e-8:
-        #Solve for the optimal amount in
+        # Solve for the optimal amount in
         def func(amount_in):
-            return Pool.getMarginalPriceSwapRiskyIn(amount_in) - m
+            return pool.getMarginalPriceSwapRiskyIn(amount_in) - m
+
         # If the sign is the same for the bounds of the possible trades, this means that the arbitrager can empty the pool while maximizing his profit (the profit may still be negative, even though maximum)
         if (np.sign(func(EPSILON)) != np.sign(func(1 - R1 - EPSILON))):
             optimal_trade = scipy.optimize.brentq(func, EPSILON, 1 - R1 - EPSILON)
         else:
             optimal_trade = 1 - R1
         assert optimal_trade >= 0
-        amount_out, _ = Pool.virtualSwapAmountInRisky(optimal_trade)
-        #The amount of the riskless asset we get after making the swap must be higher than the value in the riskless asset at which we obtained the amount in on the market
-        profit = amount_out - optimal_trade*m
+        amount_out, _ = pool.virtualSwapAmountInRisky(optimal_trade)
+        # The amount of the riskless asset we get after making the swap must be higher than the value in the riskless asset at which we obtained the amount in on the market
+        profit = amount_out - optimal_trade * m
         if profit > 0:
-            _, _ = Pool.swapAmountInRisky(optimal_trade)
-    
-    #If the price of buying epsilon of the risky asset is below the market price, we buy the optimal amount of the risky asset in the CFMM and immediately sell it on the market = **swap amount in riskless** in the CFMM.
+            _, _ = pool.swapAmountInRisky(optimal_trade)
+
+    # If the price of buying epsilon of the risky asset is below the market price, we buy the optimal amount of the risky asset in the CFMM and immediately sell it on the market = **swap amount in riskless** in the CFMM.
     elif price_buy_risky < m - 1e-8:
         def func(amount_in):
-            return m - Pool.getMarginalPriceSwapRisklessIn(amount_in)
+            return m - pool.getMarginalPriceSwapRisklessIn(amount_in)
+
         # If the sign is the same for the bounds of the possible trades, this means that the arbitrager can empty the pool while maximizing his profit (the profit may still be negative, even though maximum)
-        if (np.sign(func(EPSILON)) != np.sign(func((K+k-R2)/gamma - EPSILON))):
-            optimal_trade = scipy.optimize.brentq(func, EPSILON, (K+k-R2)/gamma - EPSILON)
-        else: 
-            optimal_trade = K- R2
-        assert optimal_trade >=0
-        amount_out, _ = Pool.virtualSwapAmountInRiskless(optimal_trade)
-        #The amount of risky asset we get out times the market price must result in an amount of riskless asset higher than what we initially put in the CFMM 
-        profit = amount_out*m - optimal_trade
+        if (np.sign(func(EPSILON)) != np.sign(func((K + k - R2) / gamma - EPSILON))):
+            optimal_trade = scipy.optimize.brentq(func, EPSILON, (K + k - R2) / gamma - EPSILON)
+        else:
+            optimal_trade = K - R2
+        assert optimal_trade >= 0
+        amount_out, _ = pool.virtualSwapAmountInRiskless(optimal_trade)
+        # The amount of risky asset we get out times the market price must result in an amount of riskless asset higher than what we initially put in the CFMM
+        profit = amount_out * m - optimal_trade
         if profit > 0:
-            _, _ = Pool.swapAmountInRiskless(optimal_trade)
+            _, _ = pool.swapAmountInRiskless(optimal_trade)
--- modules/cfmm.py
+++ modules/cfmm.py
@@ -1,29 +1,28 @@
-'''
+"""
 Contains the necessary AMM logic.
-'''
+"""
 
 import math
-from math import nan
 from math import inf
 import scipy
 from scipy.stats import norm
 from scipy import optimize
-import matplotlib.pyplot as plt
 import numpy as np
 
 from modules.utils import nonnegative, quantilePrime, blackScholesCoveredCallSpotPrice
 
 EPSILON = 1e-8
 
-class CoveredCallAMM():
-    '''
+
+class CoveredCallAMM(object):
+    """
     A class to represent a two-tokens AMM with the covered call trading function.
 
     Attributes
     ___________
 
     reserves_risky: float
-        the reserves of the AMM pool in the risky asset 
+        the reserves of the AMM pool in the risky asset
     reserves_riskless: float
         the reserves of the AMM pool in the riskless asset
     tau: float
@@ -34,173 +33,180 @@ class CoveredCallAMM():
         the volatility for this pool, scaled to be consistent with the unit of tau (annualized if tau is in years etc)
     invariant: float
         the invariant of the CFMM
-    '''
+    """
 
-    def __init__(self, initial_x, K, sigma, tau, fee):
-        '''
+    def __init__(self, initial_x, k, sigma, tau, fee):
+        """
         Initialize the AMM pool with a starting risky asset reserve as an
         input, calculate the corresponding riskless asset reserve needed to
         satisfy the trading function equation.
-        '''
+        """
         self.reserves_risky = initial_x
-        self.K = K
-        self.sigma = sigma 
+        self.K = k
+        self.sigma = sigma
         self.tau = tau
         self.initial_tau = tau
         self.invariant = 0
-        self.reserves_riskless = self.K*norm.cdf(norm.ppf(1-initial_x) - self.sigma*np.sqrt(self.tau))
+        self.reserves_riskless = self.K * norm.cdf(norm.ppf(1 - initial_x) - self.sigma * np.sqrt(self.tau))
         self.fee = fee
-        self.accured_fees = [0,0]
+        self.accured_fees = [0, 0]
 
-    def getRisklessGivenRisky(self, risky): 
-        return self.invariant + self.K*norm.cdf(norm.ppf(1 - risky) - self.sigma*np.sqrt(self.tau))
+    def getRisklessGivenRisky(self, risky):
+        return self.invariant + self.K * norm.cdf(norm.ppf(1 - risky) - self.sigma * np.sqrt(self.tau))
 
     def getRisklessGivenRiskyNoInvariant(self, risky):
-        return self.K*norm.cdf(norm.ppf(1 - risky) - self.sigma*np.sqrt(self.tau))
+        return self.K * norm.cdf(norm.ppf(1 - risky) - self.sigma * np.sqrt(self.tau))
 
     def getRiskyGivenRiskless(self, riskless):
-        return 1 - norm.cdf(norm.ppf((riskless - self.invariant)/self.K) + self.sigma*np.sqrt(self.tau))
+        return 1 - norm.cdf(norm.ppf((riskless - self.invariant) / self.K) + self.sigma * np.sqrt(self.tau))
 
     def swapAmountInRisky(self, amount_in):
-        '''
+        """
         Swap in some amount of the risky asset and get some amount of the riskless asset in return.
 
         Returns: 
 
         amount_out: the amount to be given out to the trader
         effective_price_in_risky: the effective price of the executed trade
-        '''
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma*amount_in)
+        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma * amount_in)
         amount_out = self.reserves_riskless - new_reserves_riskless
         self.reserves_risky += amount_in
         self.reserves_riskless -= amount_out
         assert nonnegative(new_reserves_riskless)
-        #Update invariant
-        self.invariant = self.reserves_riskless - self.getRisklessGivenRiskyNoInvariant(self.reserves_risky) 
-        effective_price_in_riskless = amount_out/amount_in
+        # Update invariant
+        self.invariant = self.reserves_riskless - self.getRisklessGivenRiskyNoInvariant(self.reserves_risky)
+        effective_price_in_riskless = amount_out / amount_in
         return amount_out, effective_price_in_riskless
 
-    def virtualSwapAmountInRisky(self, amount_in): 
-        '''
-        Perform a swap and then revert the state of the pool. 
+    def virtualSwapAmountInRisky(self, amount_in):
+        """
+        Perform a swap and then revert the state of the pool.
 
-        Returns: 
-        
-        amount_out: the amount that the trader would get out given the amount in 
-        effective_price_in_riskless: the effective price the trader would pay for that trade denominated in the riskless asset
-        '''
+        Returns:
+
+        amount_out: the amount that the trader would get out given the amount in
+        effective_price_in_riskless: the effective price the trader would pay for that
+        trade denominated in the riskless asset
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma*amount_in)
+        new_reserves_riskless = self.getRisklessGivenRisky(self.reserves_risky + gamma * amount_in)
         if new_reserves_riskless <= 0 or math.isnan(new_reserves_riskless):
             return 0, 0
         assert nonnegative(new_reserves_riskless)
         amount_out = self.reserves_riskless - new_reserves_riskless
         if amount_in == 0:
             effective_price_in_riskless = inf
-        else: 
-            effective_price_in_riskless = amount_out/amount_in
+        else:
+            effective_price_in_riskless = amount_out / amount_in
         return amount_out, effective_price_in_riskless
 
     def swapAmountInRiskless(self, amount_in):
-        '''
+        """
         Swap in some amount of the riskless asset and get some amount of the risky asset in return.
 
         Returns:
 
         amount_out: the amount to be given to the trader
-        effective_price_in_riskless: the effective price the trader actually paid for that trade denominated in the riskless asset
-        '''
+        effective_price_in_riskless: the effective price the trader actually paid for that trade
+        denominated in the riskless asset
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma*amount_in)
+        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma * amount_in)
         assert nonnegative(new_reserves_risky)
         amount_out = self.reserves_risky - new_reserves_risky
         assert nonnegative(amount_out)
         self.reserves_riskless += amount_in
         self.reserves_risky -= amount_out
-        #Update invariant
+        # Update invariant
         self.invariant = self.reserves_riskless - self.getRisklessGivenRiskyNoInvariant(self.reserves_risky)
         if amount_in == 0:
             effective_price_in_riskless = inf
-        else: 
-            effective_price_in_riskless = amount_in/amount_out
+        else:
+            effective_price_in_riskless = amount_in / amount_out
         return amount_out, effective_price_in_riskless
 
-    def virtualSwapAmountInRiskless(self, amount_in): 
-        '''
-        Perform a swap and then revert the state of the pool. 
+    def virtualSwapAmountInRiskless(self, amount_in):
+        """
+        Perform a swap and then revert the state of the pool.
 
-        Returns: 
+        Returns:
 
-        amount_out: the amount that the trader would get out given the amount in 
-        effective_price_in_riskless: the effective price the trader would pay for that trade denominated in the riskless asset
-        '''
+        amount_out: the amount that the trader would get out given the amount in
+        effective_price_in_riskless: the effective price the trader would pay for that
+        trade denominated in the riskless asset
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma*amount_in)
+        new_reserves_risky = self.getRiskyGivenRiskless(self.reserves_riskless + gamma * amount_in)
         if new_reserves_risky <= 0 or math.isnan(new_reserves_risky):
             return 0, 0
         assert nonnegative(new_reserves_risky)
         amount_out = self.reserves_risky - new_reserves_risky
         if amount_out == 0:
             effective_price_in_riskless = inf
-        else: 
-            effective_price_in_riskless = amount_in/amount_out
+        else:
+            effective_price_in_riskless = amount_in / amount_out
         return amount_out, effective_price_in_riskless
 
-
     def getSpotPrice(self):
-        '''
+        """
         Get the current spot price (ie "reported price" using CFMM jargon) of
         the risky asset, denominated in the riskless asset, only exact in the
         no-fee case.
-        '''
+        """
         return blackScholesCoveredCallSpotPrice(self.reserves_risky, self.K, self.sigma, self.tau)
 
     def getMarginalPriceSwapRiskyIn(self, amount_in):
-        '''
+        """
         Returns the marginal price after a trade of size amount_in (in the
         risky asset) with the current reserves (in RISKLESS.RISKY-1).
-        See https://arxiv.org/pdf/2012.08040.pdf 
-        '''
+        See https://arxiv.org/pdf/2012.08040.pdf
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        R = self.reserves_risky 
-        k = self.invariant
-        K = self.K  
+        r = self.reserves_risky
+        K = self.K
         sigma = self.sigma
         tau = self.tau
-        return gamma*K*norm.pdf(norm.ppf(float(1 - R - gamma*amount_in)) - sigma*np.sqrt(tau))*quantilePrime(1 - R - gamma*amount_in)
+        return gamma * K * norm.pdf(norm.ppf(float(1 - r - gamma * amount_in)) - sigma * np.sqrt(tau)) * quantilePrime(
+            1 - r - gamma * amount_in)
 
     def getMarginalPriceSwapRisklessIn(self, amount_in):
-        '''
+        """
         Returns the marginal price after a trade of size amount_in (in the
         riskless asset) with the current reserves (in RISKLESS.RISKY-1)
-        See https://arxiv.org/pdf/2012.08040.pdf  
-        '''
+        See https://arxiv.org/pdf/2012.08040.pdf
+        """
         assert nonnegative(amount_in)
         gamma = 1 - self.fee
-        R = self.reserves_riskless 
-        k = self.invariant
-        K = self.K  
+        R = self.reserves_riskless
+        invariant = self.invariant
+        K = self.K
         sigma = self.sigma
         tau = self.tau
-        if ((gamma * norm.pdf(norm.ppf(float((R + gamma*amount_in - k)/K)) + sigma*np.sqrt(tau))*quantilePrime((R + gamma*amount_in - k)/K)*(1/K)) < EPSILON):
-            #Infinity
+        if ((gamma * norm.pdf(norm.ppf(float((R + gamma * amount_in - invariant) / K)) + sigma * np.sqrt(tau)) *
+             quantilePrime((R + gamma * amount_in - invariant) / K) * (1 / K)) < EPSILON):
+            # Infinity
             return 1e8
-        else: 
-            return  1/(gamma * norm.pdf(norm.ppf(float((R + gamma*amount_in - k)/K)) + sigma*np.sqrt(tau))*quantilePrime((R + gamma*amount_in - k)/K)*(1/K))
+        else:
+            return 1 / (gamma * norm.pdf(
+                norm.ppf(float((R + gamma * amount_in - invariant) / K)) + sigma * np.sqrt(tau)) * quantilePrime(
+                (R + gamma * amount_in - invariant) / K) * (1 / K))
 
-    def getRiskyReservesGivenSpotPrice(self, S):
-        '''
+    def getRiskyReservesGivenSpotPrice(self, s):
+        """
         Given some spot price S in the no-fee case, get the risky reserves corresponding to that
-        spot price by solving the S = -y' = -f'(x) for x. 
-        '''
+        spot price by solving the S = -y' = -f'(x) for x.
+        """
+
         def func(x):
-            return S - blackScholesCoveredCallSpotPrice(x, self.K, self.sigma, self.tau)
+            return s - blackScholesCoveredCallSpotPrice(x, self.K, self.sigma, self.tau)
+
         sol = scipy.optimize.root(func, self.reserves_risky)
         reserves_risky = sol.x[0]
         return reserves_risky
